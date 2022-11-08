321435188f62136b792ef6c61a909eadf611264d
JeremySmits <97687791+JeremySmitss@users.noreply.github.com>
2022-11-08 15:15:12 +0100

    Add files via upload

---
+++ items.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('lockpick', 'Lockpick', 40, 0, 1),
+	('thermite', 'Thermiet', 14, 0, 1),
+                      ('net_cracker', 'Hackerstoestel', 500, 0, 1)
+;
